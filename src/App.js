c58dd4573c55962a9b00ad305f12ddbfe040dc12
jasabira <jasbircoro.dev@gmail.com>
2023-05-12 22:54:13 -0300

    Vista de Menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import CreateUser from './components/CreateUser'
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-       <CreateUser/>
+       <Menu/>
     </div>
   );
 }
