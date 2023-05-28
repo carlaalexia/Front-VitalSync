cfd91510d61d87efd259c590e468b42303609c31
Carla Alexia Marquez <camarquez@gyl.local>
2023-05-28 15:43:06 -0300

    probando login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Public from './routing/Public';
-import Login from './Pages/Login'
-import CreateUser from './Pages/Pacient/CreateUser';
-import EditProfile from './Pages/Pacient/EditProfile';
-import ViewProfile from './Pages/Pacient/ViewProfile';
-import ViewMedAppoint from './Pages/Appoints/ViewMedAppoint';
-import CreateMedAppoint from './Pages/Appoints/CreateMedAppoint';
-import Admin from './Pages/SuperAdmin/Admin';
-import ListMed from './Pages/SuperAdmin/ListMed';
-import Nav from './components/Nav';
-import NavUser from './components/NavUser';
-import NavAdmin from './components/NavAdmin';
-import MedReview from './Pages/MedRewiew';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Public from "./routing/Public";
+import Login from "./Pages/Login";
+import CreateUser from "./Pages/Pacient/CreateUser";
+import EditProfile from "./Pages/Pacient/EditProfile";
+import ViewProfile from "./Pages/Pacient/ViewProfile";
+import ViewMedAppoint from "./Pages/Appoints/ViewMedAppoint";
+import CreateMedAppoint from "./Pages/Appoints/CreateMedAppoint";
+import Admin from "./Pages/SuperAdmin/Admin";
+import ListMed from "./Pages/SuperAdmin/ListMed";
+import Nav from "./components/Nav";
+import NavUser from "./components/NavUser";
+import NavAdmin from "./components/NavAdmin";
+import MedReview from "./Pages/MedRewiew";
 
 function App() {
+  let navigation = null;
+  const userRole = localStorage.getItem("userRole");
+
+  if (userRole === "PACIENTE") {
+    navigation = <NavUser />;
+  } else if (userRole === "Admin") {
+    navigation = <NavAdmin />;
+  } else {
+    navigation = <Nav />; // Aquí puedes utilizar tu componente Nav principal
+  }
+
   return (
     <Router>
       <div>
-        <NavUser/> {/* Mover el componente Nav aquí */}
+        {navigation}
         <Routes>
           <Route path="/" element={<Navigate to="/homePage" replace />} />
           <Route path="/homePage" element={<Public />} />
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/CreateUser" element={<CreateUser />} />
-          <Route path="/Admin" element={<Admin/>}/>
-          <Route path="/Alist" element={<ListMed/>}/>
-          <Route path="/Profile" element={<EditProfile/>} />
-          <Route path="/ViewProfile" element={<ViewProfile/>} />
-          <Route path="/Turnos" element={<ViewMedAppoint/>}/>
-          <Route path="/PedirTurno" element={<CreateMedAppoint/>} />
-          <Route path="/Profesionales" element={<MedReview/>} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/Alist" element={<ListMed />} />
+          <Route path="/Profile" element={<EditProfile />} />
+          <Route path="/ViewProfile" element={<ViewProfile />} />
+          <Route path="/Turnos" element={<ViewMedAppoint />} />
+          <Route path="/PedirTurno" element={<CreateMedAppoint />} />
+          <Route path="/Profesionales" element={<MedReview />} />
         </Routes>
       </div>
     </Router>
@@ -39,9 +55,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
--- src/Servicio/ServiceInicioSesion.js
+++ src/Servicio/ServiceInicioSesion.js
@@ -3,14 +3,24 @@ import axios from "axios";
 export async function login(email, password) {
   try {
     const response = await axios.post(
-      "http://localhost:8080/vitalsync/usuario/login",
+      "http://localhost:8080/vitalsync/auth/login",
       {
         email: email,
         clave: password,
       }
     );
-    return response.data;
+
+    console.log("Respuesta:", response); // Imprime la respuesta completa en la consola
+
+    if (response.status === 200) {
+      const role = response.data; // Obtiene el rol del usuario desde la respuesta
+      console.log("Rol del usuario:", role); // Imprime el rol del usuario en la consola
+      return role;
+    } else {
+      throw new Error("Error en la solicitud");
+    }
   } catch (error) {
+    console.error("Error:", error); // Imprime el error en la consola
     throw error;
   }
 }
